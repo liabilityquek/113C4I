8f4902a6b1ffed14466059a061334d88ccdeae1d
Harold <haroldquekhere@gmail.com>
2023-09-16 05:46:11 +0800

    amend the useEffect in dashboard.components into a single useEffect and insert loading states

--- src/components/dashboard.component.jsx
+++ src/components/dashboard.component.jsx
@@ -9,59 +9,19 @@ export default function DashBoardFigures() {
   const [loading, setLoading] = useState(false);
 
   const [driver, setDriver] = useState([
-    { name: 'Present', value: 0 },
-    { name: 'Deferred', value: 0 },
+    { name: 'Present', value: 0, url: `${NEXT_PUBLIC_NEXTAUTH_URL}/api/get-driver-count` },
+    { name: 'Deferred', value: 0, url: `${NEXT_PUBLIC_NEXTAUTH_URL}/api/get-driver-count` },
   ]);
 
   const [vehicle, setVehicle] = useState([
-    { name: '5-Ton', value: 0 },
-    { name: 'OUV', value: 0 },
-    { name: 'SOUV', value: 0 },
-    { name: 'CP', value: 0 },
-    { name: 'FSD', value: 0 },
-    { name: 'PSD', value: 0 },
-    { name: 'GP', value: 0 },
+    { name: '5-Ton', value: 0, url: `${NEXT_PUBLIC_NEXTAUTH_URL}/api/get-vehicle-count` },
+    { name: 'OUV', value: 0, url: `${NEXT_PUBLIC_NEXTAUTH_URL}/api/get-vehicle-count` },
+    { name: 'SOUV', value: 0, url: `${NEXT_PUBLIC_NEXTAUTH_URL}/api/get-vehicle-count` },
+    { name: 'CP', value: 0, url: `${NEXT_PUBLIC_NEXTAUTH_URL}/api/get-vehicle-count` },
+    { name: 'FSD', value: 0, url: `${NEXT_PUBLIC_NEXTAUTH_URL}/api/get-vehicle-count` },
+    { name: 'PSD', value: 0, url: `${NEXT_PUBLIC_NEXTAUTH_URL}/api/get-vehicle-count` },
+    { name: 'GP', value: 0, url: `${NEXT_PUBLIC_NEXTAUTH_URL}/api/get-vehicle-count` },
   ]);
-  
-  useEffect(() => {
-    const fetchIndividualDriverData = async (name, index) => {
-      try {
-        const response = await fetch(`${NEXT_PUBLIC_NEXTAUTH_URL}/api/get-driver-count`)
-        const data = await response.json()
-        console.log(`data: ${data}`)
-        setDriver((prev) => {
-          const newArr = [...prev];
-          newArr[index].value = data;
-          return newArr
-        })
-      } catch (e) {
-        console.error(`Error fetching driver data: ${e}`)
-      }
-    }
-    driver.forEach((item, index) => {
-      fetchIndividualDriverData(item.name, index)
-    })
-  }, [])
-
-  useEffect(() => {
-    const fetchIndividualVehicleData = async (name, index) => {
-      try {
-        const response = await fetch(`${NEXT_PUBLIC_NEXTAUTH_URL}/api/get-vehicle-count`);
-        // const response = await fetch(`${NEXT_PUBLIC_NEXTAUTH_URL}/api/get-vehicle-${name.toLowerCase().replace('-', '')}-count`);
-        const data = await response.json();
-        setVehicle((prev) => {
-          const newArr = [...prev];
-          newArr[index].value = data;
-          return newArr
-        })
-      } catch (e) {
-        console.error(`Error fetching vehicle data: ${e}`)
-      }
-    }
-    vehicle.forEach((item, index) => {
-      fetchIndividualVehicleData(item.name, index)
-    })
-  }, [])
 
   const [data, setData] = useState([
     {
@@ -74,57 +34,84 @@ export default function DashBoardFigures() {
       category: 'Vehicles',
       stat: '0',
       data: vehicle,
-      url: `${NEXT_PUBLIC_NEXTAUTH_URL}/api/get-driver-count`,
+      url: `${NEXT_PUBLIC_NEXTAUTH_URL}/api/get-vehicle-count`,
     },
 
   ])
+
   useEffect(() => {
+    const fetchIndividualData = async (iterableArr, dataFunction) => {
+      for (const [index, item] of iterableArr.entries()) {
+        try {
+          const response = await fetch(item.url)
+          // const response = await fetch(`${NEXT_PUBLIC_NEXTAUTH_URL}/api/get-${url}-${item.name.toLowerCase().replace('-', '')}-count`)
+          const data = await response.json()
+          dataFunction(prev => {
+            const newArr = [...prev]
+            newArr[index].value = data
+            return newArr
+          })
+        } catch (err) {
+          console.log(`Error fetching driver data: ${e}`)
+        }
+      }
+    }
     const fetchData = async () => {
+      setLoading(true)
+      await fetchIndividualData(driver, setDriver)
+      await fetchIndividualData(vehicle, setVehicle)
       const newData = [];
-      try {
       for (let item of data) {
+        try {
           const response = await fetch(item.url)
           const statData = await response.text();
           newData.push({ ...item, stat: statData })
-        } }
+        }
         catch (e) {
           console.error('Error fetching data: ', e)
         }
-        setData(newData)
       }
-      fetchData()
-    },[])
+      setData(newData)
+      setLoading(false)
+    }
 
+    fetchData()
+  }, [])
 
   return (
-    <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <Grid numItemsSm={2} numItemsLg={3} className="gap-6">
-        {data.map((item) => (
-          <Card key={item.category}>
-            <Title>{item.category}</Title>
-            <Flex
-              justifyContent="start"
-              alignItems="baseline"
-              className="space-x-2"
-            >
-              <Metric>{item.stat}</Metric>
-              {console.log(`stat: ${typeof(item.stat)}`)}
-              <Text>Total Count</Text>
-            </Flex>
-            <Flex className="mt-6">
-              <Text>Field</Text>
-              <Text className="text-right">Count</Text>
-            </Flex>
-            <BarList
-              data={item.data}
-              valueFormatter={(number) =>
-                Intl.NumberFormat('us').format(number).toString()
-              }
-              className="mt-2"
-            />
-          </Card>
-        ))}
-      </Grid>
-    </main>
+    <>
+      
+        <main className="p-4 md:p-10 mx-auto max-w-7xl">
+          <Grid numItemsSm={2} numItemsLg={3} className="gap-6">
+            {data.map((item) => (
+              <Card key={item.category}>
+                <Title>{item.category}</Title>
+                <Flex
+                  justifyContent="start"
+                  alignItems="baseline"
+                  className="space-x-2"
+                >
+                  {loading ? 'Loading...' : <Metric>{item.stat}</Metric>}
+                  <Text>Total Count</Text>
+                </Flex>
+                <Flex className="mt-6">
+                  <Text>Field</Text>
+                  <Text className="text-right">Count</Text>
+                </Flex>
+                {loading ? 'Loading...' :
+                  <BarList
+                    data={item.data}
+                    valueFormatter={(number) =>
+                      Intl.NumberFormat('us').format(number).toString()
+                    }
+                    className="mt-2"
+                  />
+                }
+              </Card>
+            ))}
+          </Grid>
+        </main>
+      
+    </>
   );
 }
