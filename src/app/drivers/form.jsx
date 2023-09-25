6471f2979871e87bb85092ddaad717ac06d64838
Harold <haroldquekhere@gmail.com>
2023-09-25 22:29:40 +0800

    fix the edit driver details by removing useState and use the setValue from ract hook form

--- src/app/drivers/form.jsx
+++ src/app/drivers/form.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Fragment, useState, useEffect } from "react";
+import { useState, useEffect,  } from "react";
 import { useForm } from "react-hook-form";
 
 const NEXT_PUBLIC_BASEURL = process.env.NEXT_PUBLIC_BASEURL;
@@ -13,52 +13,40 @@ const AmendDriverDetailsForm = ({
   userId,
   setServerError,
 }) => {
-  const { handleSubmit, register } = useForm();
-
-  const [driverState, setDriverState] = useState({
-    rank: "",
-    name: "",
-    contact: "",
-    kin: "",
-    kinContact: "",
-    relationship: "",
-    availability: "",
-  });
-
+  const { handleSubmit, register, setValue, watch, formState: { errors } } = useForm();
+  
   useEffect(() => {
     if (driver) {
-      setDriverState({
-        rank: driver.rank,
-        name: driver.name,
-        contact: driver.contact,
-        kin: driver.next_of_kin_name,
-        kinContact: driver.next_of_kin_contact,
-        relationship: driver.relationship,
-        availability: driver.availability,
-      });
+      setValue('rank', driver.rank);
+      setValue('name', driver.name);
+      setValue('contact', driver.contact);
+      setValue('kin', driver.next_of_kin_name);
+      setValue('kinContact', driver.next_of_kin_contact);
+      setValue('relationship', driver.relationship);
+      setValue('availability', driver.availability);
     }
-  }, [driver]);
+  }, [driver, setValue]);
 
-  const handleChange = (e) => {
-    const value = e.target.value.toUpperCase();
-    setDriverState({
-      ...driverState,
-      [e.target.name]: value,
-    });
-  };
+  const watchedRank = watch('rank');
+  const watchedName = watch('name');
+  const watchedContact = watch('contact');
+  const watchedKin = watch('kin');
+  const watchedKinContact = watch('kinContact');
+  const disable = !watchedKin || !watchedName || !watchedRank || !watchedContact || watchedContact.length < 8 || !watchedKinContact || watchedKinContact.length < 8;
 
-  const disable =
-    !driverState.rank ||
-    !driverState.name ||
-    !driverState.contact ||
-    !driverState.kin ||
-    !driverState.kinContact ||
-    driverState.contact.length < 8 ||
-    driverState.kinContact.length < 8;
-  // !driverState.availability;
+  const handleNumericInput = (e) => {
+    e.target.value = e.target.value.replace(/[^0-9]/g, "");
+};
+
+  //Debugging States
+  // useEffect(() => {
+  //   console.log('setValue', setValue);
+  //   console.log('disable', disable); 
+  //   console.log('form errors:', errors); 
+  // }, [setValue, disable, errors]);
 
   const onSubmit = async (data) => {
-    console.log(`data: ${JSON.stringify(data, null, 2)}`);
+    // console.log(`data: ${JSON.stringify(data, null, 2)}`);
     try {
       setLoading(true);
       const response = await fetch(
@@ -95,6 +83,7 @@ const AmendDriverDetailsForm = ({
     }
   };
 
+  if(!driver) return setLoading(true)
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
@@ -104,58 +93,50 @@ const AmendDriverDetailsForm = ({
             <input
               {...register("rank", { required: true })}
               type="text"
-              onChange={handleChange}
               className="w-full py-2 px-4 rounded-md"
-              value={driverState.rank}
               style={{ textTransform: "uppercase" }}
             />
+            {errors.rank && <span>Rank is required</span>}
           </label>
         </div>
         <div className="mb-4 w-full border-2 border-gray-600 rounded-md">
           <input
             {...register("name", { required: true })}
             type="text"
-            onChange={handleChange}
             className="w-full py-2 px-4 rounded-md"
-            value={driverState.name}
             style={{ textTransform: "uppercase" }}
           />
+          {errors.name && <span>Name is required</span>}
         </div>
-        {/* <div className="mb-4 w-full border-2 border-gray-600 rounded-md">
-                      <input
-                        required
-                        type="driverState.contact"
-                        name="contact"
-                        onChange={handleChange}
-                        className="w-full py-2 px-4 rounded-md"
-                        value={driverState.contact}
-                        pattern="^[0-9]{8}$"
-                        title="Contact must be 8 digits"
-                      />
-                    </div> */}
+        <div className="mb-4 w-full border-2 border-gray-600 rounded-md">
+            <input
+              {...register("contact", { required: true, maxLength: 8, pattern: "^[0-9]{8}$" })}
+              type="text"
+              onInput={handleNumericInput}
+              className="w-full py-2 px-4 rounded-md"
+              style={{ textTransform: "uppercase" }}
+            />
+            {errors.contact && <span>Contact No must be 8 numeric digits</span>}
+          </div>
         <div className="mb-4 w-full border-2 border-gray-600 rounded-md">
           <input
             {...register("kin", { required: true })}
             type="text"
-            onChange={handleChange}
             className="w-full py-2 px-4 rounded-md"
-            value={driverState.kin}
             style={{ textTransform: "uppercase" }}
           />
+          {errors.kin && <span>Next of kin is required</span>}
         </div>
-        {/* <div className="mb-4 w-full border-2 border-gray-600 rounded-md">
-                      <input
-                        required
-                        type="driverState.kinContact"
-                        name="kinContact"
-                        onChange={handleChange}
-                        className="w-full py-2 px-4 rounded-md"
-                        value={driverState.kinContact}
-                        style={{ textTransform: "uppercase" }}
-                        pattern="^[0-9]{8}$"
-                        title="Next of Kin Contact must be 8 digits"
-                      />
-                    </div> */}
+        <div className="mb-4 w-full border-2 border-gray-600 rounded-md">
+          <input
+            {...register("kinContact", { required: true, maxLength: 8, pattern: "^[0-9]{8}$" })}
+            type="text"
+            onInput={handleNumericInput}
+            className="w-full py-2 px-4 rounded-md"
+            style={{ textTransform: "uppercase" }}
+          />
+          {errors.kinContact && <span>Next of Kin contact no must be 8 numeric digits</span>}
+          </div>
         <div className="mb-4 w-full border-2 border-gray-600 rounded-md relative"></div>
 
         <div className="mt-4 flex justify-center">
